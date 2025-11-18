70f31c5bedc6389b2162d9669399b1ba56882037
marcelorodrigoantunes <marcelo.rodrigo.antunes@hotmail.com>
2025-11-18 10:22:54 -0300

    Estruturas Condicionais 02 else

--- maratona_java/src/DevJava05.java
+++ maratona_java/src/DevJava05.java
@@ -1,5 +1,19 @@
 public class DevJava05 {
     public static void main(String[] args) {
-        
+        int idade = 15;
+        boolean isAutorizadoComprarBebida = idade >=18;
+
+        if(isAutorizadoComprarBebida) {
+            System.out.println("Autorizado a comprar bebida alcólica");
+        }
+
+        if(!isAutorizadoComprarBebida){
+            System.out.println("Não autorizado a comprar bebida alcólica");
+        }
+        boolean c = false;
+        if (c==true){
+            System.out.println("Dentro do que nunca deve ser feito");
+        }
+        System.out.println("Fora do if");
     }
 }
