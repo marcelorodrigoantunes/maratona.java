ae57ef353822f49da3363a6bf3ac5feaa18f051e
marcelorodrigoantunes <marcelo.rodrigo.antunes@hotmail.com>
2025-11-18 11:18:51 -0300

    Duas formas

--- maratona_java/src/DevJavaElseIf05.java
+++ maratona_java/src/DevJavaElseIf05.java
@@ -1,6 +1,24 @@
 public class DevJavaElseIf05 {
     public static void main(String[] args) {
 
+        /* outra opção
+        
+         int idade = 18;
+        String categoria;
+
+        if (idade < 15){
+            categoria = "Categoria infantil";
+        }
+        else if (idade >= 15 && idade < 18){
+            categoria = "Categoria juvenil";
+        }else {
+            categoria = "Categoria Adulto";
+        }
+        System.out.println(categoria);
+    }
+         */
+
+
         int idade = 18;
 
         if (idade < 15){
