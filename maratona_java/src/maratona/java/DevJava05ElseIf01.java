c76c4d8b6990ba0f06c8d6e81ef608e708ccb864
marcelorodrigoantunes <marcelo.rodrigo.antunes@hotmail.com>
2025-11-18 12:20:11 -0300

    Operador ternario ? = pergunta : se nao

--- maratona_java/src/maratona/java/DevJava05ElseIf01.java
+++ maratona_java/src/maratona/java/DevJava05ElseIf01.java
@@ -2,11 +2,16 @@ package maratona.java;
 
 public class DevJava05ElseIf01 {
     public static void main(String[] args) {
-        double salario = 6000;
+        double salario = 3000;
         String mensagemDoar = "Eu vou doar 500";
         String mensagemNaoDoar = "Não doar";
+
+        //operador ternario
+        //String resultado = salario > 500 ? mensagemDoar : mensagemNaoDoar;
+        //System.out.println(resultado);
+
         String resultado;
-        if (salario > 500){
+        if (salario > 5000){
             resultado = mensagemDoar;
         }else {
             resultado = mensagemNaoDoar;
