9eea8fde2e95d5a717d7902108935611d7096045
marcelorodrigoantunes <marcelo.rodrigo.antunes@hotmail.com>
2025-11-19 12:46:59 -0300

    Financiamento

--- maratona_java/src/maratona/java/DevJavaEstruturasDeRepeticao04.java
+++ maratona_java/src/maratona/java/DevJavaEstruturasDeRepeticao04.java
@@ -2,6 +2,13 @@ package maratona.java;
 
 public class DevJavaEstruturasDeRepeticao04 {
     public static void main(String[] args) {
-        
+        double valorTotal = 30000;
+        for (int parcela = 1; parcela <= valorTotal; parcela++) {
+            double valorParcela = valorTotal / parcela;
+            if (valorParcela < 1000) {
+                break;
+            }
+            System.out.println("Parcela " + parcela + " R$ " + valorParcela);
+        }
     }
 }
---
+++ maratona_java/src/maratona/java/DevJavaEstruturasDeRepeticao05.java
@@ -0,0 +1,14 @@
+package maratona.java;
+
+public class DevJavaEstruturasDeRepeticao05 {
+    public static void main(String[] args) {
+        double valorCarro = 30000;
+        for (int parcela = (int) valorCarro; parcela >= 1; parcela--) {
+            double valorParcela = valorCarro / parcela;
+            if (valorParcela < 1000){
+                continue;
+            }
+            System.out.println("Parcela "+ parcela + " R$ "+ valorParcela);
+        }
+    }
+}
