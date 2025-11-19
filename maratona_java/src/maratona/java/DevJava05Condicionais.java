7147880e033b2c41d50e5a9e639c6d102cf7be3d
marcelorodrigoantunes <marcelo.rodrigo.antunes@hotmail.com>
2025-11-19 09:18:52 -0300

    Switch com char

---
+++ maratona_java/src/maratona/java/DevJava05Condicionais.java
@@ -0,0 +1,45 @@
+package maratona.java;
+
+public class DevJava05Condicionais {
+    public static void main(String[] args) {
+        byte dia = 7;
+        switch (dia){
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda");
+                break;
+            case 3:
+                System.out.println("Terça");
+                break;
+            case 4:
+                System.out.println("Quarta");
+                break;
+            case 5:
+                System.out.println("Quinta");
+                break;
+            case 6:
+                System.out.println("Sexta");
+                break;
+            case 7:
+                System.out.println("Sabado");
+                break;
+            default:
+                System.out.println("Opção invalida");
+                break;
+        }
+        char sexo = 'F';
+        switch (sexo) {
+            case 'M':
+                System.out.println("Masculino");
+                break;
+            case 'F':
+                System.out.println("Feminino");
+                break;
+            default:
+                System.out.println("Dado invalido so aceito M ou F");
+                break;
+        }
+    }
+}
