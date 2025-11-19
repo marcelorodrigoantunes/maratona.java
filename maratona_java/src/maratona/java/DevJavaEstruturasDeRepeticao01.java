f230b1ee1fcd9b1cfe5ecf472f1a646808b7f80c
marcelorodrigoantunes <marcelo.rodrigo.antunes@hotmail.com>
2025-11-19 09:50:10 -0300

    contador

---
+++ maratona_java/src/maratona/java/DevJavaEstruturasDeRepeticao01.java
@@ -0,0 +1,11 @@
+package maratona.java;
+
+public class DevJavaEstruturasDeRepeticao01 {
+    public static void main(String[] args) {
+        int contador = 0;
+        while (contador < 10){
+            System.out.println(contador);
+            contador = contador + 1;
+        }
+    }
+}
