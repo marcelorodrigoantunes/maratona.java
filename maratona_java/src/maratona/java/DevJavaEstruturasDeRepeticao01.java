2723486f70da0136d84eb8b9f4eb7d340c209bb9
marcelorodrigoantunes <marcelo.rodrigo.antunes@hotmail.com>
2025-11-19 09:56:50 -0300

    primeiro

--- maratona_java/src/maratona/java/DevJavaEstruturasDeRepeticao01.java
+++ maratona_java/src/maratona/java/DevJavaEstruturasDeRepeticao01.java
@@ -4,8 +4,12 @@ public class DevJavaEstruturasDeRepeticao01 {
     public static void main(String[] args) {
         int contador = 0;
         while (contador < 10){
-            System.out.println(contador);
-            contador = contador + 1;
+            System.out.println(++contador);
         }
+        contador = 0;
+        do {
+            System.out.println("Dentro do-while "+contador);
+            contador++;
+        } while (contador < 10);
     }
 }
