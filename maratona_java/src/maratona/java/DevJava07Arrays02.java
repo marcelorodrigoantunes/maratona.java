e09d18fc7672b4d52ec293c0e48850e34c38cac9
marcelorodrigoantunes <marcelo.rodrigo.antunes@hotmail.com>
2025-11-20 11:23:56 -0300

    Length

--- maratona_java/src/maratona/java/DevJava07Arrays02.java
+++ maratona_java/src/maratona/java/DevJava07Arrays02.java
@@ -3,8 +3,12 @@ package maratona.java;
 public class DevJava07Arrays02 {
     public static void main(String[] args) {
         String[] nomes = new String[3];
-        System.out.println(nomes[0]);
-        System.out.println(nomes[1]);
-        System.out.println(nomes[2]);
+        nomes[0] = "Marcelo";
+        nomes[1] = "Rodrigo";
+        nomes[2] = "Antunes";
+
+        for (int i = 0; i < nomes.length; i++){
+            System.out.println(nomes[i]);
+        }
     }
 }
