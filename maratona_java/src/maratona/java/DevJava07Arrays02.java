cb2cb7afaefb82acc90769e3020707bf772cbc78
marcelorodrigoantunes <marcelo.rodrigo.antunes@hotmail.com>
2025-11-20 11:09:14 -0300

    Arrays

---
+++ maratona_java/src/maratona/java/DevJava07Arrays02.java
@@ -0,0 +1,10 @@
+package maratona.java;
+
+public class DevJava07Arrays02 {
+    public static void main(String[] args) {
+        String[] nomes = new String[3];
+        System.out.println(nomes[0]);
+        System.out.println(nomes[1]);
+        System.out.println(nomes[2]);
+    }
+}
